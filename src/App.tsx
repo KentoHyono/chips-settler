09de38b3a551c3dc28d8534be0a59a11a1352432
KentoHyono <keeeeento4649@icloud.com>
2026-04-05 22:20:53 -0700

    Deleted React from App.tsx to fix the error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback } from "react"
+import { useState, useRef, useCallback } from "react"
 
 import Header from "./components/Header"
 import Tabs from "./components/Tabs"
