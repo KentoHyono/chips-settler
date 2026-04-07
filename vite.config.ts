9cedc6cf88acbbed1787676e5b38bc1aa1c22cc7
KentoHyono <keeeeento4649@icloud.com>
2026-04-06 21:03:51 -0700

    Added robot.txt, sitemap.xml, and ssg plugin for SEO improvement

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import prerender from 'vite-plugin-prerender'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    prerender({
+      staticDir: path.join(__dirname, 'dist'),
+      routes: ['/'], 
+    })
+  ],
 })
