5e4934836723a062ef4ab2b43120a0b6746a97c9
KentoHyono <keeeeento4649@icloud.com>
2026-04-06 22:20:29 -0700

    eliminated vite-plugin-prerender since it was deprecated

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import prerender from 'vite-plugin-prerender'
-import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default (defineConfig)({
   plugins: [
     react(),
-    prerender({
-      staticDir: path.join(__dirname, 'dist'),
-      routes: ['/'], 
-    })
   ],
 })
